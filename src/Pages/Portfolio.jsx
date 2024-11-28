2cdc7e80854c0bc0d82f8adaee0eba26807fd6fa
taneja-garvit <145535000+taneja-garvit@users.noreply.github.com>
2024-11-28 19:27:55 +0530

    Update Portfolio.jsx

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -8,12 +8,12 @@ function Portfolio({portfolioRef}) {
         <h3 className='font-bold text-xl'>_______ P O R T F O L I O</h3>
         <h1 className='text-3xl sm:text-6xl font-bold'>Here are are some of our samples provided</h1>
         <div className='gap-x-8 gap-y-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 place-items-center'>
-          <PortfolioCard src="Taskmate.mp4" title='Taskmate' desc="Task management application" />
-          <PortfolioCard src="nimbi.mp4" title='Nimbi' desc="Animated Crypto project" />
-          <PortfolioCard src="paper_portfolio.mp4" title='Paper Portfolio' desc="Animated Portfolio website" />
+          <PortfolioCard src={'https://res.cloudinary.com/dser8edfz/video/upload/v1728362435/aurora_p7gfhu.mp4'} title='Send Me Hugs' desc="Fundraiser" />
+          <PortfolioCard src="nimbi.mp4" title='Nimbi' desc="Crypto project" />
+          <PortfolioCard src={'https://res.cloudinary.com/dser8edfz/video/upload/v1732801557/evhsbgkusbw5mh1gbbxu.mp4'} title='Nika' desc="Portfolio website" />
           <PortfolioCard src="video4.mp4" title='AI_Bank' desc="Payment Invoicing Service" />
           <PortfolioCard src="brainwave.mp4" title='Brainwave' desc="AI powered website" />
-          <PortfolioCard src="skillwallah.mp4" title='Skill Wallah' desc="Learning Management System" />
+          <PortfolioCard src={'https://res.cloudinary.com/dser8edfz/video/upload/v1732801226/hknlccoaofu7jeb5myfz.mp4'} title='Unibit' desc="Blockchain based Game" />
         </div>
       </div>
     </div>
